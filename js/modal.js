984abe99151dc500f4bd660d82e3a35da98fd94f
WebsterPineda <webster.farid@gmail.com>
2019-04-26 22:56:16 -0600

    Diseños

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
-	var modal = document.getElementById('Mymodal');
-	modal.style.display = "block";
+	setTimeout(mostrarModal(), 1500);
 });
+
+var modal = document.getElementById('Mymodal');
+
+//$("#btnIngresar").click(Mostrar);
+
+function Ingresar(){
+	var usuario = document.getElementById('txtUsuario').value;
+	if (usuario.trim() === "")
+	{
+		console.log("campo vacio");
+		return false;
+	}
+	alert("Bienvenido " + usuario);
+	setTimeout(OcultarModal(), 1500);
+}
+
+function mostrarModal() {
+	modal.style.display = "show";
+	$("#Mymodal").fadeIn(750);
+}
+
+function OcultarModal(){
+	modal.style.display = "hide";
+	$("#Mymodal").delay(10).fadeOut(450);
+}
